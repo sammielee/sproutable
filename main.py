2b516233ea778701f5c087cace55e71661b5b1b1
Sammie Lee <sammie.a.lee@gmail.com>
2021-04-24 19:37:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ from flask import Flask, render_template
 # Create the Flask App object
 app = Flask(__name__, template_folder='templates')
 
+users = []
 
 # When the default html page is requested
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def add_user():
+    un = request.form['un']
+    user = {
+    'un': un,
+        'plants': []
+    }
 
 
 if __name__ == '__main__':
